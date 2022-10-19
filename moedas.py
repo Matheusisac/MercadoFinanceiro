d06ea0c49046894c78c42cf844e1d97fcf74c731
Matheusisac <45320600+Matheusisac@users.noreply.github.com>
2022-10-19 14:19:19 -0300

    Update moedas.py

--- moedas.py
+++ moedas.py
@@ -13,19 +13,19 @@ date = []
 i = 0
 moeda = ["USD", "EUR", "CAD", "AUD", "ARS", "CNY", "JPY", "BTC","ETH", "MXN", "PEN", "UYU", "COP", "BOB"]
 for i in range(0, len(moeda)):
-    dados = requests.get(f'https://economia.awesomeapi.com.br/json/last/{moeda[i]}')
+    dados = requests.get(f'https://economia.awesomeapi.com.br/json/daily/{moeda[i]}/15')
     dados = dados.json()
-    dados = dados[moeda[i] + "BRL"]
-    code.append(dados["code"])
-    name.append(dados["name"])
-    name[i] = name[i].replace('/Real Brasileiro', '')
-    high.append(dados["high"])
-    high[i] = high[i].replace('.', ',')
-    low.append(dados["low"])
-    low[i] = low[i].replace('.', ',')
-    var.append(dados["varBid"])
-    var[i] = var[i].replace('.', ',')
-    date.append(dados['create_date'])
+    #dados = dados[moeda[i] + "BRL"]
+    for j in range(0,15):
+        code.append(dados[0]["code"])
+        name.append(dados[0]["name"])
+        name[i] = name[i].replace('/Real Brasileiro', '')
+        high.append(dados[j]["high"])
+        high[j] = high[j].replace('.', ',')
+        low.append(dados[j]["low"])
+        low[j] = low[j].replace('.', ',')
+        var.append(dados[j]["varBid"])
+        var[j] = var[j].replace('.', ',')
+        date.append(dados[0]['create_date'])
 data = {'Nome da Moeda':name, 'Sigla':code, 'Maior preço':high, 'Menor preço':low, 'variação':var, 'data da Modificação':date}
 table = pd.DataFrame(data)
-print(table)
